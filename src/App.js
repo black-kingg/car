c2b4c03f2c3ccb480d5f7f2322c5be3c275897d6
Mubarak <mudet04@gmail.com>
2023-09-24 23:37:56 +0100

    #

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import "./style.css";
+import Ground from "./components/ground";
+
+function CarShow() {
+	return (
+		<>
+			<OrbitControls
+				target={[0, 0.35, 0]}
+				maxPolarAngle={1.45}
+			/>
+
+			<PerspectiveCamera
+				makeDefault
+				fov={50}
+				position={[3, 2, 5]}
+			/>
+
+			<color
+				args={[0, 0, 0]}
+				attach="background"
+			/>
+			<spotLight
+				color={[1, 0.25, 0.7]}
+				intensity={1.5}
+				angle={0.6}
+				penumbra={0.5}
+				position={[5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+
+			<spotLight
+				color={[0.14, 0.5, 1]}
+				intensity={2}
+				angle={0.6}
+				penumbra={0.5}
+				position={[-5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+
+			<Ground />
+		</>
+	);
+}
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Suspense fallback={null}>
+			<Canvas shadows>
+				<CarShow />
+			</Canvas>
+		</Suspense>
+	);
 }
 
 export default App;
